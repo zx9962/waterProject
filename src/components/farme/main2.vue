f1be5fde508a9186f35a9c58b023de724d5057da
zhu64321 <3311586032@qq.com>
2023-09-13 20:47:45 +0800

    张国鑫

---
+++ src/components/farme/main2.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class='main2'>
+      <div id="main2"></div>
+    </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+export default {
+  name: 'main2',
+  // import引入组件才能使用
+  components: {},
+  props: {},
+  data () {
+    return {
+
+    }
+  },
+  // 计算属性
+  computed: {},
+  // 监听data中的数据变化
+  watch: {},
+  // 方法集合
+  methods: {
+
+  },
+  // 生命周期，创建完成时（可以访问当前this实例）
+  created () {
+
+  },
+  // 生命周期：挂载完成时（可以访问DOM元素）
+  mounted () {
+    var myChart2 = echarts.init(document.getElementById('main2'))
+    myChart2.setOption({
+      title: {
+        text: '年龄段'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['50后', '60后', '70后', '80后', '90后', '00后'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: 'Direct',
+          type: 'bar',
+          barWidth: '15%',
+          data: [10, 52, 200, 334, 390, 230]
+        }
+      ]
+    })
+  },
+  beforeCreate () { }, // 生命周期：创建之前
+  beforeMount () { }, // 生命周期：挂载之前
+  beforeUpdate () { }, // 生命周期：更新之前
+  updated () { }, // 生命周期：更新之后
+  beforeDestroy () { }, // 生命周期：销毁之前
+  destroyed () { }, // 生命周期：销毁完成
+  activated () { } // 如果页面有keep-alive缓存功能，这个函数会触发执行
+}
+</script>
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-// import homePage from '@/views/home/homePage'
+import portrayal from '@/views/portrayal/portrayal'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   redirect: '/home'
-    // },
-    // {
-    //   path: '/',
-    //   name: 'homePage',
-    //   component: homePage
-    // }
+    {
+      path: '/portrayal',
+      name: 'portrayal',
+      component: portrayal
+    }
   ]
 })
