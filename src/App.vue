43ed5ab2f31fd5a8d48f375cb2a72af2037f824d
zx9962 <zy2905593394@163.com>
2023-09-13 10:42:32 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,6 @@ export default {
 @import 'css/base.css';
 @import 'css/reset.css';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f4f5fb;
 }
 </style>
--- src/views/home/homePage.vue
+++ src/views/home/homePage.vue
@@ -1,9 +1,50 @@
 <template>
   <div id="homePage">
-    <h2>{{msg}}</h2>
-    <input type="text">
-    <!-- <from v-model="val" :attr="check" @checked="check = $event"></from> -->
-    <from v-model="val" :attr.sync="check"></from>
+    <div class="h-left">
+      <div class="l-title"></div>
+      <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+    </div>
+    <div class="h-right">
+      <div class="r-title"></div>
+    </div>
   </div>
 </template>
 
@@ -23,7 +64,7 @@ export default {
   created () {},
   // 生命周期:挂载完成时(可以访问DOM元素)
   mounted () {
-    this.add()
+    // this.add()
   },
   // 计算属性
   computed: {},
@@ -31,6 +72,12 @@ export default {
   watch: {},
   // 方法集合
   methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    },
     async add () {
       const res = await this.$axios.get('http://127.0.0.1:2112/data')
       console.log(res)
@@ -40,5 +87,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+#homePage{
+  display: flex;
+  .h-left{
+    width: 200px;
+    height: 100vh;
+    background-color: #1a1c28;
+    .l-title{
+      height: 64px;
+      background-color: #4174f7;
+    }
+  }
+  .h-right{
+    flex: 1;
+    .r-title{
+      height: 64px;
+      background-color: #fff;
+    }
+  }
+}
 </style>
