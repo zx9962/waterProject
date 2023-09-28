bd1b489fff8432e0310507cfb56122d0e3847643
zx9962 <zy2905593394@163.com>
2023-09-28 09:46:11 +0800

    张晓宇9.28

--- src/views/login/children/denglu.vue
+++ src/views/login/children/denglu.vue
@@ -4,15 +4,15 @@
       <h2>用户登录</h2>
     </div>
     <div class="bot d-flex flex-direction-column" @keyup.enter="login()">
-        <input type="text" placeholder="请输入用户名或手机号码" v-model.trim='mobile'>
-        <input type="text" placeholder="请输入密码" v-model.trim="pwd">
-      <div class="keeppwd">
-        <el-checkbox v-model="checked"><span class="remember">记住密码</span></el-checkbox>
-      </div>
-        <button  class="submit" @click="login()">立即登录</button>
-        <router-link to="/login/zhuce">
-          <button class="try">申请试用</button>
-        </router-link>
+    <input type="text" placeholder="请输入用户名或手机号码" v-model.trim='mobile'>
+    <input type="text" placeholder="请输入密码" v-model.trim="pwd">
+    <div class="keeppwd">
+      <el-checkbox v-model="checked"><span class="remember">记住密码</span></el-checkbox>
+    </div>
+    <button  class="submit" @click="login()">立即登录</button>
+    <router-link to="/login/zhuce">
+      <button class="try">申请试用</button>
+    </router-link>
     </div>
     <div class="bottom_mi">
       <p>忘记密码</p>
@@ -31,12 +31,29 @@ export default {
       checked: false
     }
   },
+  created () {
+    this.intValue()
+  },
   methods: {
+    intValue () {
+      let start = JSON.parse(localStorage.getItem('rePwd'))
+      if (start.checked) {
+        this.mobile = start.mobile
+        this.pwd = start.pwd
+      }
+      console.log(start)
+    },
     create () {
       this.aaa = 2
       this.houtaima()
     },
     async login () {
+      // 将是否记住密码存到本地存储里边
+      localStorage.setItem('rePwd', JSON.stringify({
+        checked: this.checked,
+        mobile: this.mobile,
+        pwd: this.pwd
+      }))
       if (this.mobile === '' || this.pwd === '') {
         this.$message.error('账号或密码不能为空！')
       } else {
